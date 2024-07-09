e37062f5c77f9f715c8f60ca578876b61c42a6cd
Anderson Fariña <inumansoul@gmail.com>
2024-07-09 10:13:36 -0400

    Routes update and users model

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('users' , 'App\Http\Controllers\UserController@index');
-Route::get('users/{id}' , 'App\Http\Controllers\UserController@show');
-Route::apiResource('companies', 'App\Http\Controllers\Companies\CompaniesController');
-Route::apiResource('customers', 'App\Http\Controllers\Customers\CustomersController');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('companies', 'App\Http\Controllers\Companies\CompaniesController');
+    Route::apiResource('customers', 'App\Http\Controllers\Customers\CustomersController');
+});
